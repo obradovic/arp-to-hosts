0c8021eb4e9d383a4afd5de1041858505ec677f8
Zo Obradovic <ping@obradovic.com>
2020-09-10 08:46:47 -0700

    Use entry_points, Bump to 0.1.4

--- setup.py
+++ setup.py
@@ -1,21 +1,27 @@
 """ Wheel Config
 """
-import setuptools
+from setuptools import setup, find_packages
 
-with open("README.md", "r") as readme:
-    long_description = readme.read()
 
-setuptools.setup(
+def get_readme_md_contents():
+    """read the contents of your README file"""
+    with open("README.md", encoding="utf-8") as f:
+        long_description = f.read()
+        return long_description
+
+
+setup(
+    version="0.1.4",
+    install_requires=["delegator.py", "python-hosts>=1.0.0", "validators>=0.18.0"],
+    entry_points={"console_scripts": ["arp-to-hosts=arp_to_hosts:main"]},
     name="arp-to-hosts",
-    version="0.1.2",
-    scripts=["arp-to-hosts"],
+    url="https://github.com/obradovic/arp-to-hosts",
     author="Zo Obradovic",
     author_email="ping@obradovic.com",
     description="arp-scan to /etc/hosts",
-    long_description=long_description,
+    long_description=get_readme_md_contents(),
     long_description_content_type="text/markdown",
-    url="https://github.com/obradovic/arp-to-hosts",
-    packages=setuptools.find_packages(),
+    packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
